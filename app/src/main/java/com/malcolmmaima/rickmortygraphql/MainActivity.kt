7fe9218f28f772e7f2d5b13fc041a3f6a58e6d3b
malcolm maima <malcolmmaima@gmail.com>
2023-01-10 19:21:08 +0300

    Update MainActivity.kt

--- app/src/main/java/com/malcolmmaima/rickmortygraphql/MainActivity.kt
+++ app/src/main/java/com/malcolmmaima/rickmortygraphql/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.malcolmmaima.rickmortygraphql
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -13,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
